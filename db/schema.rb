476dfa2f7588f5c866e3fcc9e25422a09268bb02
Taimur Akhtar <taimur.akhtar@square63.com>
2015-08-20 12:26:04 +0500

    EmbeddedAttachment: Added embedded attachment scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818075137) do
+ActiveRecord::Schema.define(version: 20150820062602) do
 
   create_table "attachments", force: :cascade do |t|
     t.string   "image_file_name",    limit: 255
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20150818075137) do
   end
 
   add_index "attachments", ["imageable_id"], name: "index_attachments_on_imageable_id", using: :btree
+
+  create_table "embedded_attachments", force: :cascade do |t|
+    t.text     "embed_code",      limit: 65535
+    t.integer  "attachable_id",   limit: 4
+    t.string   "attachable_type", limit: 32
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "embedded_attachments", ["attachable_type", "attachable_id"], name: "index_embedded_attachments_on_attachable_type_and_attachable_id", using: :btree
 
   create_table "pdf_attachments", force: :cascade do |t|
     t.integer  "doc_id",                limit: 4
