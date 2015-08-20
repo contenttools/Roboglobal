fc58c0b98bc50ef65c292ff6455abedbdea27d50
Taimur Akhtar <taimur.akhtar@square63.com>
2015-08-20 17:34:45 +0500

    Attachment: fixed issue of show link and notices.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -22,13 +22,27 @@ class AttachmentsController < ApplicationController
 
   def create
     @attachment = Attachment.new(attachment_params)
-    @attachment.save
-    respond_with(@attachment)
+    respond_to do |format|
+      if @attachment.save
+        format.html { redirect_to @attachment, notice: 'Attachment was successfully created.' }
+        format.json { render :show, status: :created, location: @attachment }
+      else
+        format.html { render :new }
+        format.json { render json: @attachment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    @attachment.update(attachment_params)
-    respond_with(@attachment)
+    respond_to do |format|
+      if @attachment.update(attachment_params)
+        format.html { redirect_to @attachment, notice: 'Attachment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @attachment }
+      else
+        format.html { render :edit }
+        format.json { render json: @attachment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
