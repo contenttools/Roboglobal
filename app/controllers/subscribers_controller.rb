68f6d48cb6e11d9dd219db421a24d1d5e70bab71
Taimur Akhtar <taimur.akhtar@square63.com>
2015-08-31 19:34:51 +0500

    Subscriber: Added html support for subscriber create.

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,7 +1,15 @@
 class SubscribersController < ApplicationController
   def create
     @subscriber = Subscriber.new(subscriber_params)
-    @subscriber.save
+    respond_to do |format|
+      if @subscriber.save
+        format.html { redirect_to :back, notice: 'Subscriber successfully Added' }
+        format.js
+      else
+        format.html { redirect_to :back, notice: @subscriber.errors.full_messages.to_sentence }
+        format.js
+      end
+    end
   end
 
   private
