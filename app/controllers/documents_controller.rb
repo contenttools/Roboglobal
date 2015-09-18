64c1f9b5f7a7d167e6f6007570be1560e65f852d
Taimur Akhtar <taimur.akhtar@square63.com>
2015-09-18 12:45:24 +0500

    Page: Changed file for download on fact sheet click in eu index.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,6 @@
 class DocumentsController < ApplicationController
   FILE_NAME_MAPPING = {
-    "fact_sheet" => ['ROBO_Global_Robotics_and_Automation_UCITS_Index (1).pdf', 'application/pdf'],
+    "fact_sheet" => ['ROBO Global_Robotics_and_Automation_UCITS_Index[5][1].pdf', 'application/pdf'],
     "holdings" => ['Index Methodology 1.4.pdf', 'application/pdf'],
     "classifications" => ['Industry Classification 1.3.pdf','application/pdf'],
   }
