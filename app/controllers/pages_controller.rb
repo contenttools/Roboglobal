ed48ffbc043a74c6cc2a847e622757a4045028cf
Michael <mike@michaellungo.com>
2015-09-21 18:27:12 +0500

    SEO: Added SEO to blog post and static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,29 @@
 class PagesController < ApplicationController
+
+  before_filter :set_seo_options
+
+  SEO_OPTIONS = {
+    "home" => {
+      title:       "ROBO Global",
+    },
+
+    "about_us" => {
+      title:       "ABOUT US",
+    },
+
+    "us_index" => {
+      title:       "ROBO US INDEX",
+    },
+
+    "eu_index" => {
+      title:       "ROBO EU INDEX",
+    },
+
+    "legal_information" => {
+      title:       "LEGAL INFORMATION",
+    },
+  }
+
   def home
     @video = PageEmbed.home_page_embed_video
     @stock_chart = PageEmbed.home_page_stock_chart
@@ -18,4 +43,9 @@ class PagesController < ApplicationController
 
   def eu_index
   end
+
+  def set_seo_options
+    prepare_meta_tags(SEO_OPTIONS[params[:action]])
+  end
+
 end
