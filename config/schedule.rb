46c4299a1d2648242c3a604644e98237b29ae941
Hassan Akram <hassan.akram@square63.com>
2015-10-01 20:07:51 +0500

    Cron Job: Changed path of log file

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,9 @@ require 'active_support/all'
 Time.zone = 'Berlin'
 
 every 1.day, :at => Time.zone.parse('23:50 pm').utc do
-  runner "IndexRecord.fetch_data_us", :output => '/logs/cron.log'
+  runner "IndexRecord.fetch_data_us", :output => 'cron.log'
 end
 
 every 1.day, :at => Time.zone.parse('23:51 pm').utc do
-  runner "IndexRecord.fetch_data_eu", :output => '/logs/cron.log'
+  runner "IndexRecord.fetch_data_eu", :output => 'cron.log'
 end
