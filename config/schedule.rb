4731983b224204b334caac97c0970f53bc954bed
Hassan Akram <hassan.akram@square63.com>
2015-10-07 18:21:58 +0500

    Cron Job: Code refactored and added support for missing data

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,10 @@ require 'active_support/all'
 
 Time.zone = 'Berlin'
 
-every 1.day, :at => Time.zone.parse('23:50 pm').utc do
-  runner "IndexRecord.fetch_data_us", :output => 'cron.log'
+every 1.day, :at => Time.zone.parse('22:50 pm').utc do
+  runner "IndexRecord.fetch_missing_data", :output => 'cron.log'
 end
 
-every 1.day, :at => Time.zone.parse('23:51 pm').utc do
-  runner "IndexRecord.fetch_data_eu", :output => 'cron.log'
+every 1.day, :at => Time.zone.parse('23:50 pm').utc do
+  runner "IndexRecord.fetch_data", :output => 'cron.log'
 end
