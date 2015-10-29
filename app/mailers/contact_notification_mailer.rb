1fcebad0e643f9593fae61190ba039a90cc244a0
Hassan Akram <hassan.akram@square63.com>
2015-10-29 19:24:47 +0500

    Mailer: Removed email addresses from mailing list

--- app/mailers/contact_notification_mailer.rb
+++ app/mailers/contact_notification_mailer.rb
@@ -2,7 +2,7 @@ class ContactNotificationMailer < ApplicationMailer
   default from: "admin@robostox.com"
   def contact_notification(contact)
     @contact = contact
-    email = root_url.include?('roboglobal.com') ? ['erica@roboglobal.com', 'khawar.qayyum@square63.com'] : ['khawar.qayyum@square63.com', 'hassan.akram@square63.com', 'waleed.nadeem@square63.com']
+    email = ['erica@roboglobal.com']
     mail to: email, subject: "New Contact Request"
   end
 end
