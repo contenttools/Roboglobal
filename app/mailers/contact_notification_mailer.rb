c32f63ba220192afa687200de4d6f011c33ce176
Hassan Akram <hassan.akram@square63.com>
2015-10-30 11:25:23 +0500

    Mailer: Added another receiver email address

--- app/mailers/contact_notification_mailer.rb
+++ app/mailers/contact_notification_mailer.rb
@@ -2,7 +2,7 @@ class ContactNotificationMailer < ApplicationMailer
   default from: "admin@roboglobal.com"
   def contact_notification(contact)
     @contact = contact
-    email = ['erica@roboglobal.com']
+    email = ['erica@roboglobal.com', 'research@roboglobal.com']
     mail to: email, subject: "New Contact Request"
   end
 end
