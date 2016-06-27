07cfe24da2f9696db2fc251a35f18f52a9ce98b0
Mehreen Tahir <mehreen.tahir@square63.com>
2016-06-27 14:56:14 +0500

    Document: Appropriate redirections incase of document dowloading if file is absent

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,9 +3,14 @@ class DocumentsController < ApplicationController
     begin
       @file = CGI::unescape Index.pdf_file(params[:index_type], params[:category])
       return (send_file "#{Rails.root}/public" + @file.split("?").first, type: "pdf", x_sendfile: true) unless @file == "#"
-      redirect_to eu_index_url
+      redirect_to get_redirect_path
     rescue
-      redirect_to eu_index_url
+      redirect_to get_redirect_path
     end
   end
+
+  private
+    def get_redirect_path
+      params[:index_type] == "eu" ? eu_index_url : investment_case_url
+    end
 end
